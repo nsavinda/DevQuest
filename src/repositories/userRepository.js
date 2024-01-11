7c80fa92aace351dc4d0893c7a50c7418cccb900
Terance Edmonds <teranceedmonds20@gmail.com>
2024-01-11 12:20:41 +0530

    challenge 0

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,29 +1,31 @@
-import knex from "knex";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-const secretKey = "@SecretKey#123!";
-import knex_db from "../../db/db-config.js";
+import knex from 'knex';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+const secretKey = '@SecretKey#123!';
+import knex_db from '../../db/db-config.js';
 
 let _db;
 function init(db) {
-  _db = db;
+    _db = db;
 }
 
 // Update this method to get all users method in challenge1.a
 async function getUsers() {
-  return [];
+    return [];
 }
 
 //Update this method to complete challenge0.c and challenge1.b
 async function getUser(id) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .raw(
-        `
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(
+                `
         SELECT
           ut.id,
           ut.email,
           ut.gender,
+          ut.firstname,
+          ut.lastname,
           ut.image_url,
           uht.name AS hobbyName,
           uht.rate AS hobbyRate,
@@ -34,94 +36,96 @@ async function getUser(id) {
         LEFT JOIN skills ust ON ut.id = ust.userId
         WHERE ut.id = ?
         `,
-        [id]
-      )
-      .then((result) => {
-        const rows = result;
-        let hobbyExist, skillExist;
+                [id]
+            )
+            .then((result) => {
+                const rows = result;
+                let hobbyExist, skillExist;
 
-        if (rows.length === 0) {
-          resolve("User not found!");
-          return;
-        }
+                if (rows.length === 0) {
+                    resolve('User not found!');
+                    return;
+                }
 
-        const user = {
-          id: rows[0].id,
-          email: rows[0].email,
-          gender: rows[0].gender,
-          image_url: rows[0].image_url,
-          hobbies: [],
-          skills: [],
-        };
+                const user = {
+                    id: rows[0].id,
+                    email: rows[0].email,
+                    gender: rows[0].gender,
+                    firstname: rows[0].firstname,
+                    lastname: rows[0].lastname,
+                    image_url: rows[0].image_url,
+                    hobbies: [],
+                    skills: []
+                };
 
-        rows.forEach((row) => {
-          hobbyExist = false;
-          if (row.hobbyName && row.hobbyRate) {
-            user.hobbies.map((hobby) => {
-              if (hobby.name === row.hobbyName) {
-                hobbyExist = true;
-              }
+                rows.forEach((row) => {
+                    hobbyExist = false;
+                    if (row.hobbyName && row.hobbyRate) {
+                        user.hobbies.map((hobby) => {
+                            if (hobby.name === row.hobbyName) {
+                                hobbyExist = true;
+                            }
+                        });
+                        if (!hobbyExist) {
+                            user.hobbies.push({
+                                name: row.hobbyName,
+                                rate: row.hobbyRate
+                            });
+                        }
+                    }
+
+                    hobbyExist = false;
+                    if (row.skillName && row.skillRate) {
+                        user.skills.map((skill) => {
+                            if (skill.name === row.skillName) {
+                                skillExist = true;
+                            }
+                        });
+                        if (!skillExist) {
+                            user.skills.push({
+                                name: row.skillName,
+                                rate: row.skillRate
+                            });
+                        }
+                    }
+                });
+
+                resolve(user);
+            })
+            .catch((error) => {
+                console.error(error);
+                reject(error);
             });
-            if (!hobbyExist) {
-              user.hobbies.push({
-                name: row.hobbyName,
-                rate: row.hobbyRate,
-              });
-            }
-          }
-
-          hobbyExist = false;
-          if (row.skillName && row.skillRate) {
-            user.skills.map((skill) => {
-              if (skill.name === row.skillName) {
-                skillExist = true;
-              }
-            });
-            if (!skillExist) {
-              user.skills.push({
-                name: row.skillName,
-                rate: row.skillRate,
-              });
-            }
-          }
-        });
-
-        resolve(user);
-      })
-      .catch((error) => {
-        console.error(error);
-        reject(error);
-      });
-  });
+    });
 }
 
 //delete a user
 async function deleteUser(id) {
-  await knex_db.raw("PRAGMA foreign_keys = ON");
+    await knex_db.raw('PRAGMA foreign_keys = ON');
 
-  return new Promise((resolve, reject) => {
-    knex_db.raw("SELECT * FROM users WHERE id = ?", [id]).then((userFound) => {
-      if (!userFound[0]) {
-        resolve("User not found!");
-        return;
-      }
+    return new Promise((resolve, reject) => {
+        knex_db.raw('SELECT * FROM users WHERE id = ?', [id]).then((userFound) => {
+            if (!userFound[0]) {
+                resolve('User not found!');
+                return;
+            }
 
-      knex_db
-        .raw("DELETE FROM users WHERE id = ?", [id])
-        .then(() => {
-          resolve("User deleted successfully!");
-        })
-        .catch((error) => {
-          console.error(error);
-          reject(error);
+            knex_db
+                .raw('DELETE FROM users WHERE id = ?', [id])
+                .then(() => {
+                    resolve('User deleted successfully!');
+                })
+                .catch((error) => {
+                    console.error(error);
+                    reject(error);
+                });
         });
     });
-  });
 }
 
 export default {
-  getUsers,
-  init,
-  getUser,
-  deleteUser,
+    getUsers,
+    init,
+    getUser,
+    deleteUser
 };
